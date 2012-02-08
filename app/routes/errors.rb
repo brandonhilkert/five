b45fcc21cdeed335e358779f13e8646fddd5b1c6
Brandon Hilkert <brandonhilkert@gmail.com>
2012-02-07 22:40:45 -0800

    Moved app into app directory and setup file locations for views and public.

--- app/routes/errors.rb
+++ app/routes/errors.rb
@@ -1,4 +1,4 @@
 class Five
   not_found {haml :'404'}
-  error {haml :'500'}
+  error {haml :'500'}  
 end
--- five.rb
+++ five.rb
@@ -3,12 +3,23 @@ Bundler.require
 
 class Five < Sinatra::Base
   enable   :raise_errors, :sessions, :logging
-  enable   :show_exceptions  if development?
-end
+  enable   :show_exceptions if development?
 
-['config/initializers', :models, :controllers, :routes].each do |dir|
-  Dir.glob("#{dir}/*.rb").each do |rb|
-    # puts "loading #{rb}"
-    require_relative rb
+  set :public_folder, File.join(root, "app", "assets")
+  set :views, File.join(root, "app", "views")
+  # set :views, File.join(File.dirname(__FILE__), "app", "assets")
+  # set :views, Proc.new { File.join(root, "app/views") }
+
+  configure do
+    # Load MongoDB using MongoMapper
+    config_file = ::Pathname.new(File.join(root, "config", "mongo.yml"))
+
+    if config_file.file?
+      config = ::YAML.load(ERB.new(config_file.read).result)
+      log = ::Logger.new(File.join(root, "log", "mongo-#{settings.environment}.log"))
+      ::MongoMapper.setup(config, settings.environment.to_s, logger: log)
+    end
   end
 end
+
+Dir[Pathname.new("app/**/*.rb")].each {|f| require_relative f}
