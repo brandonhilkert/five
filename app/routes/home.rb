0bf9e0756adb3999594eda43a49c9747bcaf18e9
Brandon Hilkert <brandonhilkert@gmail.com>
2012-02-14 14:16:07 -0500

    Don't show session data in layout and home page.

--- app/routes/home.rb
+++ app/routes/home.rb
@@ -2,9 +2,6 @@ class Five
   get '/home' do
     redirect '/' unless signed_in?
 
-    @token = session[:access_token]
-    @secret = session[:access_secret]
-
     @account = client.verify_credentials
 
     haml :home
